d5add74b4d181c8446189c15908bec0a5d5a9a83
Mahmudjon2011 <dildooraxajayeva@gmail.com>
2025-01-12 23:18:47 +0500

    Add files via upload

---
+++ main .js
@@ -0,0 +1,16 @@
+var ism = "Mahmud "
+var yosh = 13;
+
+console.log(ism,yosh);
+
+var ism = prompt("String typedagi malumot kiriting")
+var yosh = prompt("Number typedagi malumot kiriting ")
+var boolean = prompt("Boolean typedagi malumot kiriting")
+
+
+console.log(ism,yosh,boolean);
+
+
+
+
+
